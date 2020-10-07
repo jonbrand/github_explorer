a1cfbd26a2c146d6a741bbafecf59acb32881a4a
jonbrand <jonbranaraujo18@gmail.com>
2020-10-07 16:42:40 -0300

    handling input errors

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -10,9 +14,10 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
+
 
   display: flex;
 
@@ -21,8 +26,14 @@ export const Form = styled.form`
     height: 70px;
     padding: 0 24px;
     border: 0;
-    border-radius: 5px 0 0 5px;
+    border-radius: 5px 5px 5px 5px;
     color: #3a3a3a;
+    border: 2px solid #fff;
+
+    ${(props) =>
+    props.hasError && css`
+      border-color: #c53030;
+    `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -31,18 +42,25 @@ export const Form = styled.form`
 
   button {
     width: 210px;
-    height: 70px;
+    height: 74px;
     background: #04d361;
-    border-radius: 0px 5px 5px 0px;
+    border-radius: 10px 10px 10px 10px;
     border: 0;
     color: #fff;
     font-weight: bold;
     transition: background-color 0.2s;
+    margin-left: 10px;
 
     &:hover {
       background: ${shade(0.2, '#04d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
